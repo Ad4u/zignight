e5e0ddbc5abfe344bb05ce67d7474ce61a56f1f6
Adau <adrien.aubourg@gmail.com>
2025-08-12 16:18:51 +0200

    Added error management and logging

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const log = std.log;
 
 const miniaudio = @cImport({
     @cInclude("miniaudio.h");
@@ -8,7 +9,7 @@ const miniaudio = @cImport({
 const Data = @import("data.zig");
 
 const WAIT_TIME = std.time.ns_per_ms * 20;
-const FADING_TIME = 100; // ms
+const FADING_TIME = 50; // ms
 
 const STREAM_BUFFER_SIZE = std.math.pow(usize, 2, 16);
 const METADATA_BUFFER_SIZE = std.math.pow(usize, 2, 14);
@@ -18,8 +19,7 @@ const START_SIZE = std.math.pow(usize, 2, 16);
 const Fifo = std.fifo.LinearFifo(u8, .{ .Static = FIFO_SIZE });
 
 // TODO:
-// Handle errors/try/catch
-// log to stderr
+// Handle errors in miniaudio thread from miniaudio
 // Handle logic error if playing/stopping too fast (like hit play while stopping joining for faders)
 
 const State = struct {
@@ -58,43 +58,61 @@ const State = struct {
     }
 
     fn start_updater_thread(self: *State) !void {
+        log.debug("Starting Updater thread", .{});
         self.update_thread = try std.Thread.spawn(.{}, updater, .{self});
+        log.debug("Updater thread Started", .{});
     }
 
     fn start(self: *State, idx: usize) !void {
-        self.re_start_player.reset();
-        self.re_stop.reset();
-
+        log.debug("Starting threads for streaming", .{});
+        log.debug("Requested station - idx: {} - name: {s}", .{ idx, self.stations[idx].name });
         self.active_idx = idx;
 
         self.net_thread = try std.Thread.spawn(.{}, downloader, .{self});
         self.play_thread = try std.Thread.spawn(.{}, player, .{self});
+        log.debug("Net and Play threads spawned. Waiting for buffer to be filled", .{});
         while (self.fifo.count < START_SIZE) {
             std.Thread.sleep(WAIT_TIME);
         }
+        log.debug("Buffer filled. Setting the Event for the player.", .{});
         self.re_start_player.set();
     }
 
     fn stop(self: *State) void {
+        log.debug("Stop requested", .{});
         if (self.active_idx == null) return;
 
         self.re_stop.set();
+        log.debug("Joining threads", .{});
         self.net_thread.?.join();
         self.play_thread.?.join();
         self.net_thread = null;
         self.play_thread = null;
-        try self.fifo.pump(self.fifo.reader(), std.io.null_writer);
+        log.debug("Threads joined. Emptying the stream buffer", .{});
+        self.fifo.pump(self.fifo.reader(), std.io.null_writer) catch |err| {
+            log.err("An error occured while emptying the stream buffer: {s}\n", .{@errorName(err)});
+            self.quit();
+            std.process.exit(1);
+        };
         self.active_idx = null;
+
+        self.re_start_player.reset();
+        self.re_stop.reset();
+        log.debug("Streaming stopped", .{});
     }
 
     fn quit(self: *State) void {
-        self.stop();
+        log.debug("Quit requested", .{});
         self.re_quit.set();
+
+        log.debug("Joining Updater thread", .{});
         self.update_thread.?.join();
         self.update_thread = null;
+        log.debug("Thread joined. Quit process finished", .{});
     }
 
     fn updateStations(state: *State, new: Data.ParsedData) void {
+        log.info("Update station: {s} - artist: {s} - title: {s}", .{ new.station, new.artist, new.title });
         for (state.stations) |*station| {
             if (std.mem.eql(u8, station.meta_name, new.station)) {
                 state.stations_mutex.lock();
@@ -108,18 +126,21 @@ const State = struct {
 };
 
 fn updater(state: *State) !void {
+    const tlog = log.scoped(.updater);
     const meta_url = "https://nightride.fm/meta";
 
+    tlog.debug("Updater thread spawned", .{});
     var client = std.http.Client{ .allocator = state.alloc };
     var header_buffer: [4096]u8 = undefined;
     var readbuf: [METADATA_BUFFER_SIZE]u8 = undefined;
-    const uri = try std.Uri.parse(meta_url);
+    const uri = std.Uri.parse(meta_url) catch unreachable;
 
+    tlog.debug("Opening http connection", .{});
     var request = try client.open(.GET, uri, .{ .server_header_buffer = &header_buffer });
     try request.send();
     try request.finish();
     try request.wait();
-    std.debug.print("Meta Status: {?}\n", .{request.response.status});
+    tlog.debug("http status: {?}", .{request.response.status});
 
     var polls: [1]std.posix.pollfd = undefined;
     polls[0] = .{
@@ -128,6 +149,7 @@ fn updater(state: *State) !void {
         .revents = 0,
     };
 
+    tlog.debug("Starting the continuous polling", .{});
     var buf_len: usize = 0;
     while (true) {
         if (state.re_quit.isSet()) break;
@@ -149,21 +171,28 @@ fn updater(state: *State) !void {
             parsed.deinit();
         }
     }
+
+    tlog.debug("Loop breaked by quit event", .{});
     request.deinit();
     client.deinit();
+    tlog.debug("Resources freed. Exiting thread", .{});
 }
 
 fn downloader(state: *State) !void {
+    const tlog = log.scoped(.downloader);
+    tlog.debug("Downloader thread spawned", .{});
+
     var client = std.http.Client{ .allocator = state.alloc };
     var header_buffer: [4096]u8 = undefined;
     var readbuf: [STREAM_BUFFER_SIZE]u8 = undefined;
-    const uri = try std.Uri.parse(state.stations[state.active_idx.?].url);
+    const uri = std.Uri.parse(state.stations[state.active_idx.?].url) catch unreachable;
 
+    tlog.debug("Opening http connection", .{});
     var request = try client.open(.GET, uri, .{ .server_header_buffer = &header_buffer });
     try request.send();
     try request.finish();
     try request.wait();
-    std.debug.print("Stream Status: {?}\n", .{request.response.status});
+    tlog.debug("http status: {?}", .{request.response.status});
 
     var polls: [1]std.posix.pollfd = undefined;
     polls[0] = .{
@@ -172,6 +201,7 @@ fn downloader(state: *State) !void {
         .revents = 0,
     };
 
+    tlog.debug("Starting the continuous polling", .{});
     while (true) {
         if (state.re_stop.isSet()) break;
 
@@ -183,8 +213,10 @@ fn downloader(state: *State) !void {
         }
     }
 
+    tlog.debug("Loop breaked by stop event", .{});
     request.deinit();
     client.deinit();
+    tlog.debug("Resources freed. Exiting thread", .{});
 }
 
 fn read(decoder: [*c]miniaudio.ma_decoder, buf: ?*anyopaque, bytes_to_read: usize, bytes_read: [*c]usize) callconv(.c) c_int {
@@ -208,38 +240,50 @@ fn seek(_: [*c]miniaudio.struct_ma_decoder, _: c_longlong, _: c_uint) callconv(.
 }
 
 fn player(shared: *State) !void {
-    var res: miniaudio.ma_result = 0;
-    const ver = miniaudio.ma_version_string();
-    std.debug.print("MA Version: {s}\n", .{ver});
+    const tlog = log.scoped(.player);
+    tlog.debug("Player thread spawned", .{});
 
+    var res: miniaudio.ma_result = 0;
+
+    tlog.debug("Initializing engine", .{});
     var engine: miniaudio.ma_engine = miniaudio.ma_engine{};
     res = miniaudio.ma_engine_init(null, &engine);
-    std.debug.print("Engine Init: {}\n", .{res});
+    tlog.debug("Engine init output: {}", .{res});
 
+    tlog.debug("Waiting for buffer to be filled", .{});
     shared.re_start_player.wait();
 
+    tlog.debug("Buffer filled. Initializing decoder", .{});
     var decoder: miniaudio.ma_decoder = miniaudio.ma_decoder{};
     res = miniaudio.ma_decoder_init(read, seek, shared, null, &decoder);
-    std.debug.print("Decoder Init: {}\n", .{res});
+    tlog.debug("Decoder init output: {}", .{res});
 
+    tlog.debug("Initializing sound", .{});
     var sound: miniaudio.ma_sound = miniaudio.ma_sound{};
     res = miniaudio.ma_sound_init_from_data_source(&engine, &decoder, miniaudio.MA_SOUND_FLAG_STREAM, null, &sound);
-    std.debug.print("Sound Init: {}\n", .{res});
+    tlog.debug("Sound init output: {}", .{res});
 
+    tlog.debug("Starting sound", .{});
     miniaudio.ma_sound_set_fade_in_milliseconds(&sound, 0, 1, FADING_TIME);
-    _ = miniaudio.ma_sound_start(&sound);
+    res = miniaudio.ma_sound_start(&sound);
+    tlog.debug("Sound started", .{});
 
     shared.re_stop.wait();
+    tlog.debug("Received stop event. Stopping sound", .{});
 
     miniaudio.ma_sound_set_fade_in_milliseconds(&sound, 1, 0, FADING_TIME);
     std.Thread.sleep(std.time.ns_per_ms * FADING_TIME);
-    _ = miniaudio.ma_sound_stop(&sound);
-    _ = miniaudio.ma_sound_uninit(&sound);
-    _ = miniaudio.ma_decoder_uninit(&decoder);
-    _ = miniaudio.ma_engine_uninit(&engine);
+    res = miniaudio.ma_sound_stop(&sound);
+    tlog.debug("Sound stopped. Output: {}", .{res});
+
+    tlog.debug("Deinit Miniaudio resources", .{});
+    miniaudio.ma_sound_uninit(&sound);
+    res = miniaudio.ma_decoder_uninit(&decoder);
+    miniaudio.ma_engine_uninit(&engine);
+    tlog.debug("Miniaudio resources freed. Exiting thread", .{});
 }
 
-pub fn main() !void {
+pub fn main() void {
     var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
     const alloc = switch (builtin.mode) {
         .Debug, .ReleaseSafe => debug_allocator.allocator(),
@@ -250,21 +294,24 @@ pub fn main() !void {
         _ = debug_allocator.deinit();
     };
 
-    var state = try State.init(alloc);
+    var state = State.init(alloc) catch |err| {
+        log.err("Couldn't initialize application: {s}\n", .{@errorName(err)});
+        std.process.exit(1);
+    };
     defer state.deinit();
-    try state.start_updater_thread();
 
-    try state.start(0);
+    state.start_updater_thread() catch |err| {
+        log.err("Couldn't launch Updater thread: {s}\n", .{@errorName(err)});
+        std.process.exit(1);
+    };
+
+    state.start(0) catch |err| {
+        log.err("Couldn't start the stream: {s}\n", .{@errorName(err)});
+    };
 
     const stdin = std.io.getStdIn().reader();
-    _ = try stdin.readByte();
+    _ = stdin.readByte() catch unreachable;
 
+    state.stop();
     state.quit();
-
-    // state.stop();
-    // _ = try stdin.readByte();
-    // try state.start(1);
-    // _ = try stdin.readByte();
-
-    // state.stop();
 }
